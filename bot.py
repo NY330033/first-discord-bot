9a2735c0a244562d0f2d3367d9c75d3ddbac7a92
Sasha <yesilver@ewha.ac.kr>
2025-03-11 19:40:58 +0900

    현재 테스트용 command가 아니고 event로 되어 있었기 때문에 test 함수를 추가하는 쪽으로 바꾸었음. command를 추가하며 처음에 추가하기로 했던 기능을 구현하기 위해, find command를 만들었음. logging을 진행하는 과정에서 이전 설정대로라면 로그가 덮어씌워져 이전 내용을 확인할 수 없기 때문에, 로그 이름에 실행 시간이 표기되도록 변경함. 따라서 로그들은 실행할 때마다 별개의 파일로 남을 것임. 때문에 gitignore 파일 내용도 수정하여 모든 로그가 포함되지 않도록 변경하였음. find command는 구현하는 중이며, 나온 에러를 기록해 두었음.
    ..

--- bot.py
+++ bot.py
@@ -1,31 +1,77 @@
 import discord
-from discord.ext.commands import Bot
+from discord.ext import commands
 from dico_token import Token
-intents = discord.Intents.all()
+from datetime import datetime
 
-client = discord.Client(intents=intents)
-# 사용자가 명령어 입력 시 !를 입력하고 명령어 입력
-client = Bot(command_prefix=commands.when_mentioned_or('!'), intents=intents)
+import logging # 로깅모듈 추가
+import traceback #트레이스백도 추가가
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG) #디버그레벨로 로깅시작
+log_filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "_discord.log" #파일이름에 날짜 시간 포함 
+handler = logging.FileHandler(filename=log_filename, encoding='utf-8', mode='w')
+#discord.log라는 파일로 기록될 것임. utf-8 인코딩을 사용함
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+#로그는 위와 같은 포맷으로 작성될 것임
+logger.addHandler(handler)
+''' 로깅 설정 끝 '''
+
+intents = discord.Intents.default()
+intents.message_content=True
+
+description = '테스트중중'
+
+bot = commands.Bot(command_prefix='/', description=description, intents=intents)
+#bot event나 eommand로 만들면 됨
 
 # on_ready는 시작할 때 한번만 실행.
-@client.event
+@bot.event
 async def on_ready():
     print('Login...')
-    print(f'{client.user}에 로그인하였습니다.')
-    print(f'ID: {client.user.name}')
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('만두랑 해파리 노는중'))
- 
-@client.event
-async def on_message(message):
-	# message.content.startswith()는 해당 문자로 시작하는 단어에 대해서
-	# 인식하여 메시지 전송. ==로 비교 시 해당 문자만 인식
+    print(f'{bot.user}에 로그인하였습니다.')
+    print(f'ID: {bot.user.name}')
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game('만두랑 해파리 연구'))
 
-    if message.content.startswith('테스트'):
-        await message.channel.senrd("{} | {}, 안녕!".format(message.author, message.author.mention))
- 
-    if message.content == '테스트':
-        # 채널에 메시지 전송
-        await message.channel.send("{} | {}, 어서오세요!".format(message.author, message.author.mention))
+# onmessage로 해둔 테스트문구 있으니까 command를 못받길래 지움!! 
 
- 
-client.run(Token)
+@bot.command()
+async def hello(ctx):
+    '''그냥 테스트용 명령어'''
+    await ctx.channel.send("테스트용 명령어입니다.")
+
+@bot.command()
+async def find(ctx, date_str: str):
+    try: 
+
+        #datetime으로 파싱
+        input_date = datetime.strptime(date_str, "%Y-%m-%d-%H")
+        logger.debug(f"input-date: {input_date}")
+
+        #메시지 검색
+        messages = [] # < 이게 지금 자꾸 에러가난다... 
+        '''
+        현재까지 겪은 에러 젠냥이의 편안한개발을 위해 기록해주기 
+        1) 비동기 제너레이터를 반환해서 리스트 변환하려고 flatten 쓰니까 그거업더염 시전
+        2) 원래 파싱할때 형식을 월-날짜 ~시로 받았더니 띄어쓰기 기준으로 구분해서 시간 누락
+        3) 플래튼 안된대서 리스트로 바꾸니까 시간대 정보가 없어서(ㅆㅂ미친아) 오류남
+        ^ 이게 왜 발생하냐면 datetime 객체 사이에 1) 시간대 정보 있는 객체랑 2) 없는 객체 섞여있어서 그렇다 함
+        그래서 해결법: 시간대정보를 추가하거나 제거해서 통일하면 된대... 
+        아마 input_date가 시간대가 없는 객체고 created_at이 시간대가 있는 객체일거래 ㅁㅊ 
+        듣고보니까 근데 납득이 됨 input_date는 내가 걍 입력한거라 시간대가 없겠지
+        근데 디코에서 검색해서 받아온거면 시간대가 있겠네? 지금 혼자 납득했음 ㅅㅂ 
+        그래서 이걸...? input_date에 시간대 정보를 추가해서 변경하면 된다는데
+        이걸 쓰려면 pytz를 쓰면 된다는데 나 잘래 여기서부터 젠냥이가 봐 (뭐지) 
+        '''
+        ans = discord.utils.find(lambda x: x.created_at < input_date, messages)
+
+        if ans:
+            await ctx.channel.send(f"조건을 만족하는 가장 나중 메시지: {ans.content}")
+        else:
+            await ctx.channel.send("조건을 만족하는 메시지가 없습니다.")
+
+    except ValueError as e:
+        logger.error(f"오류 발생: {e}")
+        logger.error("예외 발생 위치: \n"+traceback.format_exc())
+        await ctx.channel.send("입력 형식이 잘못되었습니다. YYYY-MM-DD-H와 같이 입력해주세요. ")
+
+bot.run(Token)
